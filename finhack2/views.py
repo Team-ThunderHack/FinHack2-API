cab69f0ed77c9ff093495b8b412935b07fea16fe
Ashutosh Aswani <202051215@iiitvadodara.ac.in>
2022-12-03 18:35:06 +0530

    updated for giving past recommendations

--- finhack2/views.py
+++ finhack2/views.py
@@ -2,10 +2,15 @@ import pandas as pd
 import json
 import time
 import datetime
+import calendar
+from datetime import date
 from threading import Timer
 from datetime import date,timedelta
 from breeze_connect import BreezeConnect
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 import warnings
 warnings.simplefilter("ignore")
 
@@ -90,9 +95,50 @@ def nifty(request):
         resultJSON = [check , previousDayLowPrice , previousDayHighPrice , 
                     lastPrice , openPrice , ((lastPrice/100)*100)+100]
 
-    return (json.dumps(resultJSON))
+    return  Response(json.dumps(resultJSON))
 
 @api_view(['GET'])
-def previousnifty(request):
-    ans=[]
-    return (json.dumps(ans))
+def previousRecommendations(request):
+    today = date.today()
+    back10days = today + timedelta(days=-10, hours=0)
+    tomorrow = today + timedelta(days=1, hours=0)
+
+    breeze = BreezeConnect(api_key="8%@92h9*115gI3u022K37f=G&236B893")
+    breeze.generate_session(api_secret="2t(52wI22380C482DBl%85q1Q7834t2J", 
+                            session_token="2125876")
+
+    historicalData = breeze.get_historical_data(interval="1day",
+                            from_date= str(back10days) + "T07:00:00.000Z",
+                            to_date= str(today) + "T07:00:00.000Z",
+                            stock_code="NIFTY",
+                            exchange_code="NSE",
+                            product_type="cash",
+                            expiry_date="",
+                            right="",
+                            strike_price="")
+    data = (historicalData['Success'])
+    length = len(data)
+    marketData = []
+    for entries in data:
+        openPrice = entries['open']
+        dateOfentry = entries['datetime']
+        highPrice = entries['high']
+        lowPrice = entries['low']
+        closePrice = entries['close']
+        marketData.append([openPrice,dateOfentry[0:10],highPrice,lowPrice,closePrice])
+
+    pastRecomendations = []
+    for i in range(1,length):
+        if(marketData[i][0]>marketData[i-1][3] and 
+           marketData[i][0]<marketData[i-1][2] and
+           marketData[i][2]>marketData[i-1][2]):
+            price = (int(int(float(marketData[i-1][2]))/100))*100
+            pastRecomendations.append([(marketData[i][1]),price,"PE"])
+
+        if(marketData[i][0]>marketData[i-1][3] and 
+           marketData[i][0]<marketData[i-1][2] and
+           marketData[i][3]<marketData[i-1][3]):
+            price = ((int(int(float(marketData[i-1][3]))/100))*100)+100
+            pastRecomendations.append([(marketData[i][1]),price,"CE"])
+
+    return  Response(json.dumps(pastRecomendations))
